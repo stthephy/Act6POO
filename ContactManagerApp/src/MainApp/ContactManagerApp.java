79b007755bdcb3270b735da3715b35a24605a92b
Stephy <jcordobas@unal.edu.co>
2023-06-27 08:59:32 -0500

    Proff

---
+++ ContactManagerApp/src/MainApp/ContactManagerApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MainApp;
+
+/** @author stthephy
+ */
+public class ContactManagerApp {
+
+   
+    public static void main(String[] args) {
+        MainWindow window = new MainWindow ();
+        window.setVisible(true);
+    }
+    
+}
